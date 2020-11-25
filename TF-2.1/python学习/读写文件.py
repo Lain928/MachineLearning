121b61abd51852489f0ce66a7d9c5bfe1da7a9fa
zzc <916729656@qq.com>
2020-11-25 10:20:43 +0800

    删除tf2.1

--- TF-2.1/python学习/读写文件.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-df_news = pd.read_csv('Resources/MVI_20011.txt',header = None)
-print(df_news)
-print(type(df_news))
-df_news = df_news.head(14)
-df_news.to_csv("Resources/MVI_20011.csv",index=False,header = None)
